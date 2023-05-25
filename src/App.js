e693ae71d7198e39e474cc80a730dcdc8db193c0
naimulabeer <naimul.h.abeer@gmail.com>
2023-05-25 22:08:54 +0600

    deleting records

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ import BookList from "./components/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
+  const deleteBookByID = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  };
+
   const createBook = (title) => {
     const updatedBooks = [
       ...books,
       { id: Math.round(Math.floor() * 1000), title },
     ];
+
     setBooks(updatedBooks);
   };
 
   return (
     <div className="app">
-      <BookList books={books} />
+      <BookList books={books} onDelete={deleteBookByID} />
       <BookCreate onCreate={createBook} />
     </div>
   );
